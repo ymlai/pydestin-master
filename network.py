a52c2b1ffe12c7887447f462fbe5666d6cf83f8e
ymlai <ymlai529@gmail.com>
2015-05-05 11:39:08 +0800

    Update network.py

--- network.py
+++ network.py
@@ -27,6 +27,7 @@ class Network():
         self.layers = [
             [Layer(j, num_nodes_per_layer[j], cifar_stat, self.patch_mode, self.image_type) for j in range(num_layers)]]
 
+
     def setmode(self, mode):
         self.operating_mode = mode
         for I in range(self.number_of_layers):
@@ -57,13 +58,45 @@ class Network():
                         self.network_belief['belief'] = np.hstack((np.array(self.network_belief['belief']),
                                                                   np.array(self.layers[0][i].nodes[j][k].belief).ravel()))
 
+
+
+    def update_pool_belief_exporter(self):
+       for i in range(self.lowest_layer, self.number_of_layers):
+        if (i==self.number_of_layers-1):         # Top layer no need to pool
+           self.network_belief['belief'] = np.hstack((np.array(self.network_belief['belief']),np.array(self.layers[0][i].nodes[0][0].belief).ravel()))
+        else:
+            l=len(self.layers[0][i].nodes)
+            self.pool_belief(0,l/2,0,l/2,i)          #pool belief vector from 1/4 layer
+            self.pool_belief(l/2,l,0,l/2,i)
+            self.pool_belief(0,l/2,l/2,l,i)
+            self.pool_belief(l/2,l,l/2,l,i)
+            #self.pool_belief(0,l,0,l,i)
+            
+
+
+    def pool_belief(self, row_start, row_end, col_start, col_end, layer_index):
+        temp_network_belief=np.array([0])
+        for j in range(row_start, row_end):
+                for k in range(col_start, col_end):
+                        temp_network_belief = np.array(self.layers[0][layer_index].nodes[j][k].belief).ravel()+temp_network_belief
+        self.network_belief['belief'] = np.hstack((np.array(self.network_belief['belief']),temp_network_belief.ravel()))
+        #print "temp_network_belief=np.array([0])",temp_network_belief
+        #print "self.network_belief['belief']",self.network_belief['belief']
+        #print "len",len(self.network_belief['belief'])
+        #print "=====================================================+"
+
+
+
+
+
     def dump_belief(self, num_of_images):
         total_belief_len = len(np.array(self.network_belief).ravel())
         single_belief_len = total_belief_len / num_of_images
-        print np.array(self.network_belief).ravel()
+        #print np.array(self.network_belief).ravel()
         belief = np.array(self.network_belief).reshape(
             num_of_images, single_belief_len)
         io.savemat(self.belief_file_name, belief)
 
     def clean_belief_exporter(self):
         self.network_belief['belief'] = np.array([])
+
