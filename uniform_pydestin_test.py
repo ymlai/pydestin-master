696a8b8e3ee45342b656e67a7436eac16af7b206
ymlai <ymlai529@gmail.com>
2015-06-29 13:39:01 +0800

    Update uniform_pydestin_test.py

--- uniform_pydestin_test.py
+++ uniform_pydestin_test.py
@@ -81,7 +81,7 @@ for epoch in range(5):
             else:
                 DESTIN.layers[0][L].load_input(
                     DESTIN.layers[0][L - 1].nodes, [2, 2])
-                DESTIN.layers[0][L].shared_learning()
+                DESTIN.layers[0][L].shared_learning()       #sharing centroids in each layer
                 #DESTIN.layers[0][L].do_layer_learning()
     #if epoch%10==0:
          #pickle.dump( DESTIN, open( "SharedTemp", "wb" ) )
